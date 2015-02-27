8773383bd8d0e366e4f72cc19724716725ea9c0b
cartermp <phillip-carter@jmstudios.net>
2015-02-26 23:41:53 -0800

    yes

--- IBTLUnitTests/CodeGeneratorTests.cs
+++ IBTLUnitTests/CodeGeneratorTests.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 
 namespace IBTLUnitTests
 {
+    /// <summary>
+    /// tests things
+    /// </summary>
     [TestClass]
     public class CodeGeneratorTests
     {
